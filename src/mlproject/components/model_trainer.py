910b38c4d42bcfd06d5f1aff86b6b984d9518590
ashif323 <ashifiqbal0783@gmail.com>
2025-02-21 21:20:03 +0530

    working on mlflow and adding dagshub integrated with git hub

--- src/mlproject/components/model_trainer.py
+++ src/mlproject/components/model_trainer.py
@@ -2,8 +2,8 @@ import os
 import sys
 from dataclasses import dataclass
 from urllib.parse import urlparse
-# import mlflow
-# import mlflow.sklearn
+import mlflow
+import mlflow.sklearn
 import numpy as np
 from sklearn.metrics import mean_squared_error,mean_absolute_error
 from catboost import CatBoostRegressor
@@ -22,13 +22,21 @@ from src.mlproject.exception import CustomException
 from src.mlproject.logger import logging
 from src.mlproject.utils import save_object,evaluate_models
 
+
 @dataclass
 class ModelTrainerConfig:
-    trained_model_file_path = os.path.join("artifact","model.pkl")
+    trained_model_file_path=os.path.join("artifacts","model.pkl")
 
 class ModelTrainer:
     def __init__(self):
         self.model_trainer_config=ModelTrainerConfig()
+
+    def eval_metrics(self,actual, pred):
+        rmse = np.sqrt(mean_squared_error(actual, pred))
+        mae = mean_absolute_error(actual, pred)
+        r2 = r2_score(actual, pred)
+        return rmse, mae, r2
+
     def initiate_model_trainer(self,train_array,test_array):
         try:
             logging.info("Split training and test input data")
@@ -38,7 +46,6 @@ class ModelTrainer:
                 test_array[:,:-1],
                 test_array[:,-1]
             )
-
             models = {
                 "Random Forest": RandomForestRegressor(),
                 "Decision Tree": DecisionTreeRegressor(),
@@ -48,7 +55,6 @@ class ModelTrainer:
                 "CatBoosting Regressor": CatBoostRegressor(verbose=False),
                 "AdaBoost Regressor": AdaBoostRegressor(),
             }
-
             params={
                 "Decision Tree": {
                     'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
@@ -91,13 +97,57 @@ class ModelTrainer:
             ## To get best model score from dict
             best_model_score = max(sorted(model_report.values()))
 
-
              ## To get best model name from dict
 
             best_model_name = list(model_report.keys())[
                 list(model_report.values()).index(best_model_score)
             ]
             best_model = models[best_model_name]
+
+            print("This is the best model:")
+            print(best_model_name)
+
+            model_names = list(params.keys())
+
+            actual_model=""
+
+            for model in model_names:
+                if best_model_name == model:
+                    actual_model = actual_model + model
+
+            best_params = params[actual_model]
+
+            mlflow.set_registry_uri("https://dagshub.com/ashif323/MLProject.mlflow")
+            tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+
+            # mlflow
+
+            with mlflow.start_run():
+
+                predicted_qualities = best_model.predict(X_test)
+
+                (rmse, mae, r2) = self.eval_metrics(y_test, predicted_qualities)
+
+                mlflow.log_params(best_params)
+
+                mlflow.log_metric("rmse", rmse)
+                mlflow.log_metric("r2", r2)
+                mlflow.log_metric("mae", mae)
+
+
+                # Model registry does not work with file store
+                if tracking_url_type_store != "file":
+
+                    # Register the model
+                    # There are other ways to use the Model Registry, which depends on the use case,
+                    # please refer to the doc for more information:
+                    # https://mlflow.org/docs/latest/model-registry.html#api-workflow
+                    mlflow.sklearn.log_model(best_model, "model", registered_model_name=actual_model)
+                else:
+                    mlflow.sklearn.log_model(best_model, "model")
+
+
+
 
             if best_model_score<0.6:
                 raise CustomException("No best model found")
@@ -114,5 +164,6 @@ class ModelTrainer:
             return r2_square
 
 
+
         except Exception as e:
-            raise CustomException(e,sys)    
+            raise CustomException(e,sys)
